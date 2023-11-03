f1d62e31205f2cab2af6c017dc8898b652ca2d93
dachi papashvili <115042460+Dachi-Papashvili88@users.noreply.github.com>
2023-11-03 10:52:37 +0000

    add slider on main page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,24 @@
+import ImageSlider from "../components/ImageSlider";
 const Main = () => {
-    return (
-        <div>
-            <h1>Main page</h1>
-        </div>
-    )
-}
+  const slides = [
+    { url: "./images/broken.jpg", title: "beach" },
+    { url: "./images/jesus.jpg", title: "boat" },
+    { url: "./images/The Star.jpg", title: "forest" },
+    { url: "./images/god's not dead1.jpg", title: "city" },
+    { url: "./images/god's not dead1.jpg.jpg", title: "italy" }
+  ];
+  const containerStyles = {
+    width: "100&",
+    height: "70vh",
+    margin: "0 auto"
+  };
+  return (
+    <div>
+      <div style={containerStyles}>
+        <ImageSlider slides={slides} />
+      </div>
+    </div>
+  );
+};
 
-export default Main
+export default Main;
